4024db64c8083d6ef9828960dc2f8731fb2c658f
VMc <akusov.vladimir@gmail.com>
2019-10-01 22:07:23 +0300

    Домашнее задание

--- SqlJoinsDemo/Scripts/Joins-01.sql
+++ SqlJoinsDemo/Scripts/Joins-01.sql
@@ -1,7 +1,8 @@
 ﻿USE [SqlJoinsDemo]
-/*SELECT [s].[name] AS 'Название поставщика', [c].[name] AS 'Город'
+
+SELECT [s].[name] AS 'Название поставщика', [c].[name] AS 'Город'
 FROM [Cities] AS [c] JOIN [Suppliers] AS [s] ON ([c].[id] = [s].[city_id])
-*/
+
 
 SELECT c.[name] AS 'Город', s.[name] AS 'Поставщик 1', s2.[name] AS 'Поставщик 2' 
 FROM [Cities] AS c 
--- SqlJoinsDemo/Scripts/Selects.sql
+++
@@ -1,9 +0,0 @@
-﻿USE [Suppliers]
-/*SELECT [s].[name] AS 'Название поставщика', [c].[name] AS 'Город'
-FROM [Cities] AS [c] JOIN [Suppliers] AS [s] ON ([c].[id] = [s].[city_id])*/
-
-SELECT [c].[name] AS 'Город', [s].[name] AS 'Название поставщика 1', [s2].[name] AS 'Название поставщика 2'
-FROM [Cities] AS [c]
-	JOIN [Suppliers] AS [s] ON ([c].[id] = [s].[city_id])
-	JOIN [Suppliers] AS [s2] ON ([s].[city_id] = [s2].[city_id])
-WHERE [s].[id] > [s2].[id];
