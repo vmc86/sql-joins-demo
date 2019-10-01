e5e59c633a4486266d8c73835b6312a8fdc58ee8
VMc <akusov.vladimir@gmail.com>
2019-10-01 21:45:49 +0300

    Select_byer_city

---
+++ SqlJoinsDemo/Scripts/Joins-01.sql
@@ -0,0 +1,10 @@
+﻿USE [SqlJoinsDemo]
+/*SELECT [s].[name] AS 'Название поставщика', [c].[name] AS 'Город'
+FROM [Cities] AS [c] JOIN [Suppliers] AS [s] ON ([c].[id] = [s].[city_id])
+*/
+
+SELECT c.[name] AS 'Город', s.[name] AS 'Поставщик 1', s2.[name] AS 'Поставщик 2' 
+FROM [Cities] AS c 
+	JOIN [Suppliers] AS s ON (c.id = s.city_id) 
+	JOIN [Suppliers] AS s2 ON (s.city_id = s2.city_id) 
+WHERE s.id > s2.id;
